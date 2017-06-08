76e9eeff5a370329a3d400f191712931d57a78d8
Rahul-1991 <rahul1991.c@gmail.com>
2017-06-09 01:20:21 +0530

    added static root path

--- blogger/settings.py
+++ blogger/settings.py
@@ -114,10 +114,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-CSRF_COOKIE_SECURE = False
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_ROOT = [os.path.join(BASE_DIR, 'static')]
